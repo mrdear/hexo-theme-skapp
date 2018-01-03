824128f7ea8c433ed13645442cf630b67598204e
quding <li.niu@duitang.com>
2018-01-03 22:09:53 +0800

    init

---
+++ source/js/layout/loading.js
@@ -0,0 +1,28 @@
+// loading
+// window.addEventListener('load', function() {
+    
+// });
+document.onreadystatechange = function(){
+    console.log(document.readyState)
+    if (document.readyState == 'interactive') {
+        window.setTimeout(function(){
+            disableLoad()
+        },4000)
+    }
+
+    if (document.readyState == 'complete') {
+        var page = document.getElementById('page')
+        if (page.classList.contains('js-hidden')) {
+            disableLoad()
+        }
+    }
+}
+
+function disableLoad(){
+    var
+    page = document.getElementById('page'),
+    loading = document.getElementById('page-loading');
+
+    loading.classList.add('js-hidden');
+    page.classList.remove('js-hidden');
+}

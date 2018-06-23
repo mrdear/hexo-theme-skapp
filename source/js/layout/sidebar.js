f5f6780f5e4c53686a4e88059dcd1041db5b1adc
quding <li.niu@duitang.com>
2018-06-23 17:24:43 +0800

    style

--- source/js/layout/sidebar.js
+++ source/js/layout/sidebar.js
@@ -1,8 +1,15 @@
+function isMobile() {
+    if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)))
+        return true;
+    else
+        return false;
+}
+
 window.addEventListener('load', function () {
     // 回到顶部
     (function () {
         var sidebarTocElm = document.getElementById('sidebar-toc');
-        if (sidebarTocElm) {
+        if (sidebarTocElm && !isMobile()) {
             var oldOffetTop = sidebarTocElm.offsetTop;
 
             function scrollEvent() {
